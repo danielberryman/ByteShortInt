6375aecc49fb016f72508c8b2ffd68704d960e52
Daniel Berryman <danielberryman90@gmail.com>
2019-05-25 22:19:43 -0400

    First commit

---
+++ src/com/danielberryman/Main.java
@@ -0,0 +1,32 @@
+package com.danielberryman;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // Integer has a width of 32
+        // 2 ^ 31
+        int myMin= -2_147_483_648;
+        int myMax= 2_147_483_647;
+        int myTotal= (myMin/2);
+        System.out.println("myTotal = " + myTotal);
+
+        // Byte has a width of 8
+        byte myByteMin= -128;
+        byte myByteMax= 127;
+        byte myByte= (byte) (myByteMin/2);
+        System.out.println("myByte = " + myByte);
+
+        // Short has a width of 16
+        short myShortMin= -32768;
+        short myShortMax= 32767;
+        short myShort= (short) (myShortMin/2);
+        System.out.println("myShort = " + myShort);
+
+        // Long has a width of 64
+        // 2 ^ 63
+        long myLongMin = 100L;
+        long myLongMax = 100L;
+    }
+}
